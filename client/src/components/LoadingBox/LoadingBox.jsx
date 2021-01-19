df6b2e681dd2ba5a6d9a31ee74168dea5524cbde
Bing Z <bingz@me.com>
2021-01-19 14:09:47 -0700

    added loading component

---
+++ client/src/components/LoadingBox/LoadingBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LoadingBox() {
+    return (
+        <div>
+            <i className="fa fa-spinner fa-spin"></i>
+            Loading ... 
+        </div>
+    );
+}
+
+export default LoadingBox;
