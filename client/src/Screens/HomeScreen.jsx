078dded79519b19dd7549d5fc41f399ecac653e0
Bing Z <bingz@me.com>
2021-01-19 13:23:33 -0700

    install axios pkg n fetch product in homepage

--- client/src/Screens/HomeScreen.jsx
+++ client/src/Screens/HomeScreen.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect  } from 'react';
+import axios from 'axios';
 import Product from '../components/Product';
-import data from '../data/products';
 
 function HomeScreen () {
+    const [ products, setproducts ] = useState([]);
+
+    //set loading screen 
+    const [ loading, setLoading ] = useState(false)
+
+
+
+
+    //When page first load, fetch product data from backend 
+    useEffect(() => {
+        const fetchData = async () => {
+            // deconstruct product data array from response obj 
+            const  { data } = await axios.get('/api/products');
+            // console.log(data);
+            setproducts(data)
+        }
+        fetchData();
+    }, [])
+
 	return (
 		<div>
 			<div className='row center'>
-				{data.products.map(product => <Product key={product._id} product={product} />)}
+				{ products.map(product => <Product key={product._id} product={product} />)}
 			</div>
 		</div>
 	);
