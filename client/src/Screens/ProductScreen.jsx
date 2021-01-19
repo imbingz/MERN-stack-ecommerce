9f94e4e8fc1237adab467ab9af2e9d82ba5b7702
Bing Z <bingz@me.com>
2021-01-19 14:09:28 -0700

    fixed react  eslint  error n warning

--- client/src/Screens/ProductScreen.jsx
+++ client/src/Screens/ProductScreen.jsx
@@ -4,70 +4,70 @@ import Rating from '../components/Rating';
 import data from '../data/products';
 
 function ProductScreen (props) {
-	// console.log(props);
+    // console.log(props);
 
-	//props.match.params is the url path /product/:id
-	//props.match.params.id = id (1, 2 , 3 etc... )
-	// only return the product info of the one being clicked
-	const product = data.products.find(p => p._id === props.match.params.id);
-	return (
-		<div>
+    //props.match.params is the url path /product/:id
+    //props.match.params.id = id (1, 2 , 3 etc... )
+    // only return the product info of the one being clicked
+    const product = data.products.find(p => p._id === props.match.params.id);
+    return (
+        <div>
             {/* back to result link  */}
             <Link to='/'> Back To Result</Link>
 
-			<div className='row top'>
-				{/* product image */}
-				<div className='col-2'>
-					<img className='large' src={product.image} alt={product.name} />
-				</div>
+            <div className='row top'>
+                {/* product image */}
+                <div className='col-2'>
+                    <img className='large' src={product.image} alt={product.name} />
+                </div>
 
-				{/* description */}
-				<div className='col-1'>
-					<ul>
-						<li>
-							<h1>{product.name}</h1>
-						</li>
-						<li>
-							<Rating rating={product.rating} numReviews={product.numReviews} />
-						</li>
-						<li>Price: ${product.price}</li>
-						<li>
+                {/* description */}
+                <div className='col-1'>
+                    <ul>
+                        <li>
+                            <h1>{product.name}</h1>
+                        </li>
+                        <li>
+                            <Rating rating={product.rating} numReviews={product.numReviews} />
+                        </li>
+                        <li>Price: ${product.price}</li>
+                        <li>
 							Description:
-							<p>{product.description} </p>
-						</li>
-					</ul>
-				</div>
+                            <p>{product.description} </p>
+                        </li>
+                    </ul>
+                </div>
 
-				{/* action */}
-				<div className='col-1'>
-					<div className='card car-body'>
-						<ul>
-							<li>
-								<div className='row'>
-									<div>Price </div>
-									<div className='price'>${product.price}</div>
-								</div>
-							</li>
-							<li>
-								<div className='row'>
-									<div>Status </div>
-									<div>
-										{
-											product.countInStock > 0 ? <span className='success'> In Stock</span> :
-											<span className='error'> Unavailable</span>
+                {/* action */}
+                <div className='col-1'>
+                    <div className='card car-body'>
+                        <ul>
+                            <li>
+                                <div className='row'>
+                                    <div>Price </div>
+                                    <div className='price'>${product.price}</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='row'>
+                                    <div>Status </div>
+                                    <div>
+                                        {
+                                            product.countInStock > 0 ? <span className='success'> In Stock</span> :
+                                                <span className='error'> Unavailable</span>
                                         }
-									</div>
-								</div>
-							</li>
+                                    </div>
+                                </div>
+                            </li>
                             <li>
                                 <button className='primary block'>Add to Cart</button>
                             </li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default ProductScreen;
