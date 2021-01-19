a506bea1697d83ce004989213d18a437c666b2ef
Bing Z <bingz@me.com>
2021-01-19 14:10:16 -0700

    added error msg component

---
+++ client/src/components/MessageBox/MessageBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MessageBox(props) {
+    return (
+        <div className={`alert alert-${props.variants}  ||  "info" `}>
+            {props.children}
+        </div>
+    );
+}
+
+export default MessageBox;
