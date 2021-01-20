6e622801944facf3f35920d6235793eab032862b
Bing Z <bingz@me.com>
2021-01-20 14:00:02 -0700

    added product actions file for redux

---
+++ client/src/actions/productActions.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { 
+    PRODUCT_LIST_REQUEST, 
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL
+} from '../constants/productConstants';
+
+
+export const listProducts = () => async (dispatch) => {
+    dispatch({
+        type: PRODUCT_LIST_REQUEST
+    });
+
+    try{
+        const { data } = await axios.get('/api/products');
+        dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data});
+    } catch(err) {
+        dispatch({ type: PRODUCT_LIST_FAIL, payload: err.message});
+    }
+};
