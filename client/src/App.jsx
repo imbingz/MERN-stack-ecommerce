eda704f2e8fe4180af535be62b83b45ad2eb5c6d
imbingz <bingz@me.com>
2021-01-22 21:34:59 -0700

    lowercased dir name screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import HomeScreen from './Screens/HomeScreen';
-import ProductScreen from './Screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App () {
     return (
