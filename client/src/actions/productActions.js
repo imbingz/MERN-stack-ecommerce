c2b651da3a92923fa50e6438c6de57fa2828ea0a
imbingz <bingz@me.com>
2021-01-22 21:36:25 -0700

    added product details action

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 import { 
     PRODUCT_LIST_REQUEST, 
     PRODUCT_LIST_SUCCESS,
-    PRODUCT_LIST_FAIL
+    PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants';
 
  
@@ -16,5 +19,16 @@ export const listProducts = () => async (dispatch) => {
         dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data});
     } catch(err) {
         dispatch({ type: PRODUCT_LIST_FAIL, payload: err.message});
+    }
+};
+
+export const detailsProduct = (productId) => async (dispatch) => {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
+
+    try {
+        const { data } = await axios.get(`/api/products/${productId}`);
+        dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data});
+    } catch(error) {
+        dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message});
     }
 };
