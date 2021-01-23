146afe4384cb08290338e410b0f687b28db65282
imbingz <bingz@me.com>
2021-01-22 19:20:47 -0700

    replaced fetch with redux

--- client/src/Screens/HomeScreen.jsx
+++ client/src/Screens/HomeScreen.jsx
@@ -1,34 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
 import Product from '../components/Product';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { useSelector, useDispatch } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 function HomeScreen () {
-    const [ products, setproducts ] = useState([]);
-    //set loading screen 
-    const [ loading, setLoading ] = useState(false);
-    //set error screen 
-    const [ error, setError ] = useState('');
+    //import useDispatch from react-redux
+    const dispatch = useDispatch();
+    // get all productList (products, loading,  error) from redux store using useSelector
+    // useSelector accepts a function with state as parameter. 
+    // state is what defined in store.js (combineReducers)
+    const productList = useSelector(state => state.productList);
+    const { products, loading, error } = productList;
 
     //When page first load, fetch product data from backend 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                //setLoading true when start fetching data
-                setLoading(true);
-                // deconstruct product data array from response obj 
-                const { data } = await axios.get('/api/products');
-                // console.log(data);
-                setproducts(data);
-                setLoading(false);
-            } catch(err) {
-                console.error(err);
-                setError(err.message);
-                setLoading(false);
-            }         
-        };
-        fetchData();
+        // use dispatch to replace axios product fetch and set loading, error. Make sure to call listProducts function 
+        dispatch(listProducts());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
