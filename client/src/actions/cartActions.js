54ceabc47b2fecd945d431962818e9534b963129
imbingz <bingz@me.com>
2021-01-24 12:27:36 -0700

    define cartActions

---
+++ client/src/actions/cartActions.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { CART_ADD_ITEM } from '../constants/cartConstants';
+
+// when we define an action function, it should return an async function with dispatch 
+// disptach and getState are redux-thunk functions to get access to redux store
+export const addToCart = (productId, qty) => async (dispatch, getState) => {
+    // data (product) is deconstructed from axios return data
+    const { data } = await axios.get(`/api/products/${productId}`);
+
+    //using product data to dispatch 
+    dispatch({
+        type: CART_ADD_ITEM,
+        payload: {
+            name: data.name,
+            image: data.image,
+            price: data.price,
+            countInStock: data.countInStock,
+            product: data._id,
+            qty
+        }
+    });
+};
