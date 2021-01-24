5a8bcf58da46efddc778fe9561838aaec50b4de1
imbingz <bingz@me.com>
2021-01-24 12:28:47 -0700

    added dispatch upon loading to cartScreen

--- client/src/screens/CartScreen.jsx
+++ client/src/screens/CartScreen.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../actions/cartActions';
+
 
 function CartScreen(props) {
 
     const productId = props.match.params.id;
-
     ///cart/${productId}?qty={qty}
     const qty = props.location.search ? Number(props.location.search.split('=')[1]) : 1;
+    const dispatch = useDispatch();
 
+    // on page load, check if productId, if so, dispatch addToCart action 
+    useEffect(() => {
+        if(productId) {
+            dispatch(addToCart(productId, qty));
+        }
+    }, [dispatch, productId, qty]);
+
+    
     return (
         <div>
             <h1>Cart </h1>
