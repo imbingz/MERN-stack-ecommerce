f574b2fe50e1e83c8cb7f7fe968048ad06931bce
imbingz <bingz@me.com>
2021-01-24 12:27:18 -0700

    replaced a href with Link n to

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
@@ -10,13 +10,13 @@ function App () {
             <div className='grid-container'>
                 <header className='row'>
                     <div>
-                        <a className='brand' href='/'>
+                        <Link className='brand' to='/'>
 							Zankoo
-                        </a>
+                        </Link>
                     </div>
                     <div>
-                        <a href='/cart'>Cart</a>
-                        <a href='/signin'>Sign In</a>
+                        <Link to='/cart'>Cart</Link>
+                        <Link to='/signin'>Sign In</Link>
                     </div>
                 </header>
                 <main>                  
--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Rating from '../Rating';
 
 function Product (props) {
@@ -6,13 +7,13 @@ function Product (props) {
 
     return (
         <div className='card'>
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <img className='medium' src={product.image} alt='product' />
-            </a>
+            </Link>
             <div className='card-body'>
-                <a href={`/product/${product._id}`}>
+                <Link to={`/product/${product._id}`}>
                     <h2>{product.name}</h2>
-                </a>
+                </Link>
                 {/* Rating component */}
                 <Rating rating={product.rating} numReviews={product.numReviews} />
                 <div className='price'>${product.price}</div>
