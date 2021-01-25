66f08608cba25fab425316d0a8689ae34b7f844c
imbingz <bingz@me.com>
2021-01-24 18:07:54 -0700

    del hard coded product id from seed data

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -16,7 +16,6 @@ const data = {
     ], 
     products: [
         {
-            _id: '1',
             name: 'Nike Slim Shirt',
             category: 'Shirts',
             image: '/images/p1.jpg',
@@ -28,7 +27,6 @@ const data = {
             description: 'high quality product',
         },
         {
-            _id: '2',
             name: 'Adidas Fit Shirt',
             category: 'Shirts',
             image: '/images/p2.jpg',
@@ -40,7 +38,6 @@ const data = {
             description: 'high quality product',
         },
         {
-            _id: '3',
             name: 'Lacoste Free Shirt',
             category: 'Shirts',
             image: '/images/p3.jpg',
@@ -52,7 +49,6 @@ const data = {
             description: 'high quality product',
         },
         {
-            _id: '4',
             name: 'Nike Slim Pant',
             category: 'Pants',
             image: '/images/p4.jpg',
@@ -64,7 +60,6 @@ const data = {
             description: 'high quality product',
         },
         {
-            _id: '5',
             name: 'Puma Slim Pant',
             category: 'Pants',
             image: '/images/p5.jpg',
@@ -76,7 +71,6 @@ const data = {
             description: 'high quality product',
         },
         {
-            _id: '6',
             name: 'Adidas Fit Pant',
             category: 'Pants',
             image: '/images/p6.jpg',
