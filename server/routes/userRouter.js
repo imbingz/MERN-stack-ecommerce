bb12e9a5edeef9c836e1960053f5f92cf82fb6b6
imbingz <bingz@me.com>
2021-01-24 19:36:57 -0700

    added api/users/signin route n tested in postman

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const expressAsyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
 const User = require('../models/userModel');
 const data = require('../seeds/seed');
+const generateToken = require('../utils/util');
 const userRouter = express.Router();
 
 //https://www.npmjs.com/package/express-async-handler/v/1.1.4
@@ -13,5 +15,31 @@ userRouter.get('/seed', expressAsyncHandler(async (req, res) => {
     res.send( { createdUsers });
 })
 ); 
+
+
+// /api/user/signin route 
+userRouter.post('/signin', expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email});
+    //check if there is user with given email
+    if(user) {
+        // use bcrypt to validate password
+        if(bcrypt.compareSync(req.body.password, user.password)) {
+            res.send({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user)
+            });
+
+            return;
+        }
+    }
+    // 401 unauthorized 
+    res.status(401).send({ message: 'Invalid email or password'});
+})
+); 
+
+
 
 module.exports = userRouter;
