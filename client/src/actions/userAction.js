5e42c8ba80ea72c0c8df157ab57433f56ac7abde
imbingz <bingz@me.com>
2021-01-28 15:17:49 -0700

    added userActions

---
+++ client/src/actions/userAction.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS } from '../constants/userConstants';
+
+export const signin = (email, password) => async (dispatch) => {
+    dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password}});
+
+    try{
+        // use axios for http post request when user signing in 
+        const { data } = await axios.post('/api/users/signin', { email, password});
+        // if success, dispatch success and set payload to data 
+        dispatch({ type: USER_SIGNIN_SUCCESS, payload:  data });
+        // save data to localStorage
+        localStorage.setItem('userInfo', JSON.stringify(data));
+
+    }catch(error) {
+        // if error, dispatch FAIL, set payload to error message 
+        dispatch({ type: USER_SIGNIN_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message});
+    }
+};
