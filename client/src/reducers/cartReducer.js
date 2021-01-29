f2bb3fb2923bbca88f3b85b7b49380138d99e091
imbingz <bingz@me.com>
2021-01-28 21:41:47 -0700

    added saveShippingAddress reducer

--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -1,5 +1,5 @@
 
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstants';
 
 export const cartReducer = (state = { cartItems: []}, action ) => {
     switch (action.type) {
@@ -22,6 +22,11 @@ export const cartReducer = (state = { cartItems: []}, action ) => {
             return {
                 ...state,
                 cartItems: state.cartItems.filter( x => x.product !== action.payload)
+            };
+        case CART_SAVE_SHIPPING_ADDRESS:
+            return {
+                ...state,
+                shippingAddress: action.payload
             };
         default:
             return state;
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -17,6 +17,9 @@ const initialState = {
         cartItems: localStorage.getItem('cartItems')
             ? JSON.parse(localStorage.getItem('cartItems'))
             : [],
+        shippingAddress: localStorage.getItem('shippingAddress')
+            ? JSON.parse(localStorage.getItem('shippingAddress'))
+            : {},
     }
 };
 
