c21f09a476c1f05ba35da4afa036ca17a6f4b759
imbingz <bingz@me.com>
2021-01-28 20:11:54 -0700

    added userRegisterReducer

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,5 +1,28 @@
-import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT } from '../constants/userConstants';
+import { 
+    USER_REGISTER_FAIL, 
+    USER_REGISTER_REQUEST, 
+    USER_REGISTER_SUCCESS, 
+    USER_SIGNIN_FAIL, 
+    USER_SIGNIN_REQUEST, 
+    USER_SIGNIN_SUCCESS, 
+    USER_SIGNOUT 
+} from '../constants/userConstants';
 
+//user RegisterReducer 
+export const userRegisterReducer = (state = {}, action) => {
+    switch(action.type) {
+        case USER_REGISTER_REQUEST:
+            return ({ loading: true});
+        case USER_REGISTER_SUCCESS:
+            return ({ loading: false, userInfo: action.payload });
+        case USER_REGISTER_FAIL:
+            return ({ loading: false, error: action.payload });
+        default:
+            return state;
+    }
+};
+
+// User SignunReducer 
 export const userSigninReducer = (state = {}, action) => {
     switch(action.type) {
         case USER_SIGNIN_REQUEST:
