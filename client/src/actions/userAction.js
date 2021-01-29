0e032350fa77d97d0590d89561178ede2bb44f04
imbingz <bingz@me.com>
2021-01-28 20:11:21 -0700

    added user register action

--- client/src/actions/userAction.js
+++ client/src/actions/userAction.js
@@ -1,5 +1,28 @@
 import axios from 'axios';
-import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT } from '../constants/userConstants';
+import { USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT } from '../constants/userConstants';
+
+
+//register action 
+export const register = (name, email, password) => async (dispatch) => {
+    dispatch({ type: USER_REGISTER_REQUEST, payload: { name, email, password}});
+
+    try{
+        // use axios for http post request when user REGISTERg in 
+        const { data } = await axios.post('/api/users/register', { name, email, password});
+        // if success, dispatch success and set payload to data 
+        dispatch({ type: USER_REGISTER_SUCCESS, payload:  data });
+        //also dispatch SignIn_Success because userSignin.userInfo is what we use to valid user
+        dispatch({ type: USER_SIGNIN_SUCCESS, payload:  data });
+        // save data to localStorage
+        localStorage.setItem('userInfo', JSON.stringify(data));
+
+    }catch(error) {
+        // if error, dispatch FAIL, set payload to error message 
+        dispatch({ type: USER_REGISTER_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message});
+    }
+};
+
+
 
 // signin action 
 export const signin = (email, password) => async (dispatch) => {
