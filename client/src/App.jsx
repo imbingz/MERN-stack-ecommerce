5aad0567ab105d4dcd3a849e189dd3f14933c1ca
imbingz <bingz@me.com>
2021-01-28 20:12:46 -0700

    added registerScreen route in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import { signout } from './actions/userAction';
 
 function App () {
@@ -65,6 +66,7 @@ function App () {
                     <Route path='/cart/:id?' component={CartScreen} />
                     <Route path='/product/:id' component={ProductScreen} />
                     <Route path='/signin' component={SigninScreen} />
+                    <Route path='/register' component={RegisterScreen} />
                     <Route exact path='/' component={HomeScreen} />
                 </main>
                 <footer className='row center'>All right reserved</footer>
