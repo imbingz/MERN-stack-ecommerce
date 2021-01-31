68a388f784d56c3d31d73682660e8b3068c6316b
imbingz <bingz@me.com>
2021-01-30 17:49:37 -0700

    added isAuth middleware to utils.js

--- server/utils/util.js
+++ server/utils/util.js
@@ -1,6 +1,7 @@
 
 const jwt = require('jsonwebtoken');
 
+// create jwt token upon user signin
 const generateToken = (user) => {
     return jwt.sign({
         _id: user._id,
@@ -15,5 +16,29 @@ const generateToken = (user) => {
 };
 
 
+// isAuth middleware that gets the token info from header and returns user object 
+const isAuth = (req, res, next) => {
+    const authorization = req.header.authorization;
+    if(authorization) {
+        const token = authorization.slice(7); //Bearer xxxxxx 
+        // use JWT to decode the token n get the user info
+        jwt.verify(token, process.env.JWT_SECRET || 'somethingsecret', (err, decode) => {
+            if(err) {
+                // if err, send 401 not authorized message
+                return res.status(401).send({message: 'Invalid token'});
+            } 
+            // decode contains the user data defined in jwt token
+            req.user = decode;
+            console.log(decode);
+            
+            // pass req.user data to the next middleware 
+            next();
+        });
+    } else {
+        return res.status(401).send({message: 'No Token'});
+    }
+};
 
-module.exports = generateToken;
+
+
+module.exports = {generateToken, isAuth};
