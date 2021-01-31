9c3a0c45a0422cf54a876092fb8045ec0590e2a9
imbingz <bingz@me.com>
2021-01-30 21:30:10 -0700

    added cart-empty in cart-reducer

--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -1,5 +1,5 @@
 
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstants';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS, CART_EMPTY } from '../constants/cartConstants';
 
 export const cartReducer = (state = { cartItems: []}, action ) => {
     switch (action.type) {
@@ -32,6 +32,11 @@ export const cartReducer = (state = { cartItems: []}, action ) => {
             return {
                 ...state,
                 paymentMethod: action.payload
+            };
+        case CART_EMPTY:
+            return {
+                ...state,
+                cartItems: []
             };
         default:
             return state;
