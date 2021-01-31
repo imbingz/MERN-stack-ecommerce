8f6b11443111362d333331d76c5399d030bae575
imbingz <bingz@me.com>
2021-01-30 17:48:53 -0700

    created order model n schema

---
+++ server/models/orderModel.js
@@ -0,0 +1,98 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema(
+    {
+        orderItems: [{
+            name: {
+                type: String,
+                required: true
+            },
+            qty: {
+                type: Number,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true
+            },
+            image: {
+                type: String,
+                required: true
+            },
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+        }],
+        shippingAddress: {
+            fullName: {
+                type: String,
+                required: true
+            },
+            address: {
+                type: String,
+                required: true
+            },
+            city: {
+                type: String,
+                required: true
+            },
+            zipcode: {
+                type: String,
+                required: true
+            },
+            country: {
+                type: String,
+                required: true
+            },
+        },
+        paymentMethod: {
+            type: String,
+            required: true
+        },
+        itemsPrice: {
+            type: Number,
+            required: true
+        },
+        shippingPrice: {
+            type: Number,
+            required: true
+        },
+        taxPrice: {
+            type: Number,
+            required: true
+        },
+        totalPrice: {
+            type: Number,
+            required: true
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        isPaid: {
+            type: Boolean,
+            default: false
+        },
+        paidAt: {
+            tppe: Date
+        },
+        isDelivered: {
+            type: Boolean,
+            default: false
+        },
+        deliveredAt: {
+            type: Date
+        }
+    }, 
+    {
+        timestamps: true
+    }
+
+);
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
