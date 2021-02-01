c7717a792e5fa8dc76467d91d72948ab94b8baa2
imbingz <bingz@me.com>
2021-02-01 14:05:25 -0700

    added order detail screen n updated app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import { signout } from './actions/userAction';
 import ShippingAddressScreen from './screens/ShippingAddressScreen.jsx';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 function App () {
     // get cart from redux store using useSelector
@@ -73,6 +74,7 @@ function App () {
                     <Route path='/shipping' component={ ShippingAddressScreen} />
                     <Route path='/payment' component={ PaymentMethodScreen} />
                     <Route path='/placeorder' component={ PlaceOrderScreen} />
+                    <Route path='/order/:id' component={ OrderScreen} />
                     <Route exact path='/' component={HomeScreen} />
                 </main>
                 <footer className='row center'>All right reserved</footer>
