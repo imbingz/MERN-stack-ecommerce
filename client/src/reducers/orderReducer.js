188af3e5d5dbd1f4b939ec8c740fd4de08bb515c
imbingz <bingz@me.com>
2021-02-01 14:04:36 -0700

    added order details reducer for redux store

--- client/src/reducers/orderReducer.js
+++ client/src/reducers/orderReducer.js
@@ -1,5 +1,6 @@
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_RESET, ORDER_CREATE_SUCCESS } from '../constants/orderConstants';
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_RESET, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_SUCCESS } from '../constants/orderConstants';
 
+// create Order reducer 
 export const orderCreateReducer = (state = {}, action) => {
     switch (action.type) {
         case ORDER_CREATE_REQUEST:
@@ -10,6 +11,21 @@ export const orderCreateReducer = (state = {}, action) => {
             return { loading: false, error: action.payload };
         case ORDER_CREATE_RESET:
             return {};
+        default:
+            return state;
+    }
+};
+
+
+// orderDetail reducer 
+export const orderDetailsReducer = (state = { loading: true, order: {}}, action ) => {
+    switch(action.type) {
+        case ORDER_CREATE_REQUEST:
+            return { loading: true };
+        case ORDER_DETAILS_SUCCESS:
+            return { loading: false, order: action.payload };
+        case ORDER_DETAILS_FAIL:
+            return { loading: false, error: action.payload };
         default:
             return state;
     }
