cbd6aba407ec8d0daa6c9b8079ea8dab73c76769
imbingz <bingz@me.com>
2021-02-02 16:00:26 -0700

    added payOrder reducer

--- client/src/reducers/orderReducer.js
+++ client/src/reducers/orderReducer.js
@@ -1,4 +1,4 @@
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_RESET, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_SUCCESS } from '../constants/orderConstants';
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_RESET, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_RESET, ORDER_PAY_SUCCESS } from '../constants/orderConstants';
 
 // create Order reducer 
 export const orderCreateReducer = (state = {}, action) => {
@@ -26,6 +26,22 @@ export const orderDetailsReducer = (state = { loading: true, order:{}}, action )
             return { loading: false, order: action.payload };
         case ORDER_DETAILS_FAIL:
             return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
+// order Payment reducer 
+export const orderPayReducer = (state = {}, action ) => {
+    switch(action.type) {
+        case ORDER_PAY_REQUEST:
+            return {loading: true, order:{}};
+        case ORDER_PAY_SUCCESS:
+            return { loading: false, success: true };
+        case ORDER_PAY_FAIL:
+            return { loading: false, error: action.payload };
+        case ORDER_PAY_RESET:
+            return {};
         default:
             return state;
     }
