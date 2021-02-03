6785478bdd0ec6864b8e9b8df62ea50ef6129a42
imbingz <bingz@me.com>
2021-02-03 15:28:46 -0700

    added order hisotry reducer

--- client/src/reducers/orderReducer.js
+++ client/src/reducers/orderReducer.js
@@ -1,4 +1,4 @@
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_RESET, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_RESET, ORDER_PAY_SUCCESS } from '../constants/orderConstants';
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_RESET, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_SUCCESS, ORDER_MINE_LIST_FAIL, ORDER_MINE_LIST_REQUEST, ORDER_MINE_LIST_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_RESET, ORDER_PAY_SUCCESS } from '../constants/orderConstants';
 
 // create Order reducer 
 export const orderCreateReducer = (state = {}, action) => {
@@ -42,6 +42,20 @@ export const orderPayReducer = (state = {}, action ) => {
             return { loading: false, error: action.payload };
         case ORDER_PAY_RESET:
             return {};
+        default:
+            return state;
+    }
+};
+
+// order history related reducer 
+export const orderMineListReducer = (state = { orders: []}, action) => {
+    switch (action.type) {
+        case ORDER_MINE_LIST_REQUEST:
+            return { loading: true };
+        case ORDER_MINE_LIST_SUCCESS:
+            return { loading: false, orders: action.payload };
+        case ORDER_MINE_LIST_FAIL:
+            return { loading: false, error: action.payload };
         default:
             return state;
     }
