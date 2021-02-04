387a1199c808db07679869904d621bbfa372cdfd
imbingz <bingz@me.com>
2021-02-04 13:25:14 -0700

    added userDeatilsReducer

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,4 +1,7 @@
 import { 
+    USER_DETAILS_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
     USER_REGISTER_FAIL, 
     USER_REGISTER_REQUEST, 
     USER_REGISTER_SUCCESS, 
@@ -34,6 +37,20 @@ export const userSigninReducer = (state = {}, action) => {
         case USER_SIGNOUT:
             return {};
 
+        default:
+            return state;
+    }
+};
+
+//User Details reducer
+export const useDetailsReducer = (state = {loading: true}, action) => {
+    switch(action.type) {
+        case USER_DETAILS_REQUEST:
+            return ({ loading: true });
+        case USER_DETAILS_SUCCESS:
+            return ({ loading: false, user: action.payload });
+        case USER_DETAILS_FAIL:
+            return ({ loading: false, error: action.payload });
         default:
             return state;
     }
