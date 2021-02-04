27c5171979530ad588b5711d66503aeff24b3e62
imbingz <bingz@me.com>
2021-02-04 13:25:29 -0700

    updated store with useDetailsReducer

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,7 +2,7 @@ import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { ProductDetailsReducer, productListReducer } from '../reducers/productReducer';
 import { cartReducer } from '../reducers/cartReducer';
-import { userRegisterReducer, userSigninReducer } from '../reducers/userReducer';
+import { useDetailsReducer, userRegisterReducer, userSigninReducer } from '../reducers/userReducer';
 import { orderCreateReducer, orderDetailsReducer, orderMineListReducer, orderPayReducer } from '../reducers/orderReducer';
 
 
@@ -36,6 +36,7 @@ const reducer = combineReducers({
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
     orderMineList: orderMineListReducer,
+    userDetails: useDetailsReducer
 });
 
 //https://extension.remotedev.io/#usage
